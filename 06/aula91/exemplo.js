49e1461acb634c670c508bbe0f76d6633b1745da
sdias-code <silviodias.ms@gmail.com>
2025-09-29 12:21:13 -0400

    aula91

---
+++ 06/aula91/exemplo.js
@@ -0,0 +1,30 @@
+// defineProperty - defineProperties
+
+function Produto(nome, preco, estoque){
+    this.nome = nome;
+    this.preco = preco;
+    //this.estoque = estoque
+
+    Object.defineProperty(this, 'estoque', {
+        enumerable: true,
+        //value: estoque,
+        //writable: true,
+        configurable: false,
+        get: function(){ // [Getter]
+            return estoque;
+        },
+        set: function(valor){ // Setter
+            if(typeof valor !== 'number') return console.log('Valor atribuído para estoque inválido');
+            estoque = valor;
+        }
+    })
+}
+
+const p1 = new Produto('Camisa', 20, 5);
+console.log(p1);
+console.log(p1.estoque);
+
+p1.estoque = 'alguma coisa';
+p1.estoque = 65;
+
+console.log(p1.estoque);
