4a7be6549f13e4088e3a314acf6f3e2782ad9ef1
sdias-code <silviodias.ms@gmail.com>
2025-10-07 12:17:14 -0400

    aula116 - webpack

---
+++ 09/aula116/public/assets/js/bundle.js
@@ -0,0 +1,11 @@
+/******/ (() => { // webpackBootstrap
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+// 1
+// 2
+// 3
+console.log('EIIIIIIIIIIIIIIIIIIIIIII');
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
