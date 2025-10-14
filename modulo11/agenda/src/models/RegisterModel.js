eed8207e127a3ad2b7b9ebef83db71b642688814
sdias-code <silviodias.ms@gmail.com>
2025-10-14 14:20:57 -0400

    aula153 - Criando usuario e validar persistencia

--- modulo11/agenda/src/models/RegisterModel.js
+++ modulo11/agenda/src/models/RegisterModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcryptjs = require('bcryptjs');
 
 const registerSchema = new mongoose.Schema({
     email: { type: String, required: true },
@@ -15,35 +16,43 @@ class Register {
         this.user = null;
     }
 
-    async register(){
-       try {
-         this.valida();
-        if(this.errors.length > 0) return;
+    async register() {
 
-        this.user = await RegisterModel.create(this.body);
-        
-       } catch (error) {
-        console.log(error);
-        this.errors.push('Erro ao cadastrar usuário. Tente novamente mais tarde.');
-        return;
-       }
+        this.valida();
+        if (this.errors.length > 0) return;
+
+        this.userExists();
+        if (this.errors.length > 0) return;
+
+        try {
+            const salt = bcryptjs.genSaltSync();
+
+            this.body.password = await bcryptjs.hash(this.body.password, salt);
+
+            this.user = await RegisterModel.create(this.body);
+
+        } catch (error) {
+            console.log(error);
+            this.errors.push('Erro ao cadastrar usuário. Tente novamente mais tarde.');
+            return;
+        }
     }
 
-    valida(){
+    valida() {
         this.cleanUp();
 
         // Email precisa ser válido
-        if(!validator.isEmail(this.body.email)) this.errors.push('Email inválido');
+        if (!validator.isEmail(this.body.email)) this.errors.push('Email inválido');
         // Senha entre 3 e 50 caracteres
-        if(this.body.password.length < 3 || this.body.password.length > 50) {
+        if (this.body.password.length < 3 || this.body.password.length > 50) {
             this.errors.push('A senha precisa ter entre 3 e 50 caracteres.');
         }
 
     }
 
-    cleanUp(){
-        for(const key in this.body){
-            if(typeof this.body[key] !== 'string'){
+    cleanUp() {
+        for (const key in this.body) {
+            if (typeof this.body[key] !== 'string') {
                 this.body[key] = '';
             }
         }
@@ -51,6 +60,17 @@ class Register {
             email: this.body.email,
             password: this.body.password
         };
+    }
+
+    async userExists() {
+        try {
+            this.user = await RegisterModel.findOne({ email: this.body.email });
+            if (this.user) this.errors.push('Usuário já existe.');
+        } catch (error) {
+            this.errors.push('Erro ao verificar usuário. Tente novamente mais tarde.');
+            return;
+        }
+
     }
 
 }
